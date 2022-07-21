f35d51564f8da2cdd5c2c7cbc381648e3592fe55
Vincent Depaul <vincentomo@Vincents-MacBook-Air.local>
2022-07-21 16:22:01 +0300

    first deployment prototype

---
+++ src/app/chatbotapp/utils.py
@@ -0,0 +1,13 @@
+class Util:
+    """This class acts as source for creating helper functions consumed by the chat application.
+    """
+
+    @classmethod
+    def incoming_event_callback(self) -> str:
+        """This callback is  triggered when incoming messages arrive.
+        """ 
+        print("Incoming data")
+
+        # TODO: Complete the implemtatation
+
+        return ''
