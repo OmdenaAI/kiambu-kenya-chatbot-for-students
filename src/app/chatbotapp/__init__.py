a5217b29f88c758ab5f272c36887d6853a6195d2
Emaan <23100276@lums.edu.pk>
2022-07-26 16:26:00 +0500

    UI made

--- src/app/chatbotapp/__init__.py
+++ src/app/chatbotapp/__init__.py
@@ -1,14 +1,14 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from flask import Flask
 from flask_socketio import SocketIO
 
-load_dotenv()
+#load_dotenv()
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
-app.config['SECRET_KEY'] = 'Myswebwkjbejber'
+#app.config['SECRET_KEY'] = 'Myswebwkjbejber'
 
 socketio = SocketIO(app)
 
